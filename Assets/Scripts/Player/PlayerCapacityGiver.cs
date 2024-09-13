3fb2df3aef1445acb04e7be5dceac282c25c4a26
Thomas Rogulski <rogulskith@gmail.com>
2024-09-13 21:21:35 +0200

    fix compile error

--- Assets/Scripts/Player/PlayerCapacityGiver.cs
+++ Assets/Scripts/Player/PlayerCapacityGiver.cs
@@ -1,23 +1,17 @@
-using MiniJam67.Player;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-//using static MiniJam67.Player.IPlayerCapacity;
 
-public class PlayerCapacityGiver : MonoBehaviour
+namespace MiniJam167.Player
 {
-    private void OnTriggerEnter(Collider player)
+    public class PlayerCapacityGiver : MonoBehaviour
     {
+        private void OnTriggerEnter(Collider player)
+        {
 
+        }
+
+        private void OnTriggerExit(Collider player)
+        {
+
+        }
     }
-
-    private void OnTriggerExit(Collider player)
-    {
-
-    }
-
-
-
-
 }
