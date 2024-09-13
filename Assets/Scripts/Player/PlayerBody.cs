4ef81e564ad1858a8d374df4d68df2731bdd2ca2
Marwan Jerbi <je.marwan@gmail.com>
2024-09-13 21:10:40 +0200

    Player Body first scene

--- Assets/Scripts/Player/PlayerBody.cs
+++ Assets/Scripts/Player/PlayerBody.cs
@@ -5,16 +5,47 @@ namespace MiniJam67.Player
 {
 	public class PlayerBody : MonoBehaviour, IHittable
 	{
+        [SerializeField] private Rigidbody2D _rigidBody;
+		[Space]
 		[SerializeField] private float _moveSpeed;
+		[SerializeField] private int _lifePoint;
 
-		public void OnHit(IHitter hitter)
+		private Vector2 _playerMovementInput;
+		private PlayerBody _mainBody;
+
+        private void Start()
+        {
+            PlayerInput.PlayerMoved += OnPlayerMoved;
+        }
+
+        private void OnDestroy()
+        {
+            PlayerInput.PlayerMoved -= OnPlayerMoved;
+        }
+        private void OnPlayerMoved(Vector2 position)
+        {
+            _playerMovementInput = position;
+        }
+
+        private void FixedUpdate()
 		{
-			
+            _rigidBody.velocity = _playerMovementInput * _moveSpeed;
+        }
+
+        public void SetMainBody(PlayerBody mainBody)
+        {
+            _mainBody = mainBody;
+        }
+
+        public void OnHit(IHitter hitter)
+		{
+			//_mainBody.TakeDamage();
 		}
 
-		private void Move(Vector2 input)
-		{
-			
-		}
+		//private void TakeDamage(int damageAmount)
+		//{
+		//	_lifePoint = _lifePoint - damageAmount;
+
+  //      }
 	}
 }
