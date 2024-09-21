83226b84375e88a0ed23d9a1db7a30f1b416c674
Thomas Rogulski <rogulskith@gmail.com>
2024-09-21 23:58:32 +0200

    add patterns and corrupt patterns

--- Assets/Scripts/Enemy/EnemyBody.cs
+++ Assets/Scripts/Enemy/EnemyBody.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using MiniJam167.HitSystem;
-using MiniJam167.Utility;
 using UnityEngine;
 
 namespace MiniJam167.Enemy
--- Assets/Scripts/Enemy/EnemySkillSpawner.cs
+++ Assets/Scripts/Enemy/EnemySkillSpawner.cs
@@ -1,4 +1,5 @@
 using System;
+using DG.Tweening;
 using MiniJam167.Projectile;
 using MiniJam167.Utility;
 using UnityEngine;
@@ -9,12 +10,28 @@ namespace MiniJam167.Enemy
     [RequireComponent(typeof(EnemyPart))]
     public class EnemySkillSpawner : ProjectileSpawner
     {
+        [Serializable] public struct Pattern
+        {
+            public float DelayBetweenEachPart;
+            public float DelayAfterPattern;
+            public PatternPart[] Parts;
+        }
+        
+        [Serializable] public struct PatternPart
+        {
+            public Transform Point;
+            public float Rotation;
+            public float Delay;
+        }
+        
         [Serializable] public struct Spawn
         {
             public Transform[] Points;
             public Vector2[] RotationRange;
         }
         
+        [SerializeField] private Pattern[] _patterns;
+        [SerializeField] private Pattern[] _corruptPatterns;
         [SerializeField] private Spawn[] _spawns;
         [SerializeField] private float _fireRate;
         [SerializeField] private float _corruptFireRate;
@@ -24,6 +41,8 @@ namespace MiniJam167.Enemy
         
         private float _timer;
         private float _delay;
+        private bool _isCorrupted;
+        private Sequence _patternTween;
 
         private void Reset()
         {
@@ -32,7 +51,7 @@ namespace MiniJam167.Enemy
 
         private void Awake()
         {
-            _part.Initialized += OnEnabled;
+            _part.Initialized += OnInitialized;
             _part.Enabled += OnEnabled;
             _part.Disabled += OnDisabled;
             _part.Corrupted += OnCorrupted;
@@ -41,7 +60,7 @@ namespace MiniJam167.Enemy
 
         private void OnDestroy()
         {
-            _part.Initialized -= OnEnabled;
+            _part.Initialized -= OnInitialized;
             _part.Enabled -= OnEnabled;
             _part.Disabled -= OnDisabled;
             _part.Corrupted -= OnCorrupted;
@@ -66,25 +85,63 @@ namespace MiniJam167.Enemy
             }
         }
 
+        private void OnInitialized()
+        {
+            _isCorrupted = false;
+            OnEnabled();
+        }
+
         private void OnEnabled()
         {
             _delay = _fireRate == 0 ? 0 : 1f / _fireRate;
             _timer = _delay;
+            SetNewPattern();
         }
 
         private void OnDisabled(float duration)
         {
+            _patternTween?.Kill();
             _delay = 0;
         }
 
         private void OnCorrupted()
         {
+            _isCorrupted = true;
+            SetNewPattern();
             _delay = _corruptFireRate == 0 ? 0 : 1f / _corruptFireRate;
         }
 
         private void OnHidden()
         {
+            _patternTween?.Kill();
             _delay = 0;
+        }
+
+        private void SetNewPattern()
+        {
+            var patterns = _isCorrupted ? _corruptPatterns : _patterns;
+            if (patterns.Length == 0)
+                return;
+            
+            Pattern pattern = patterns.GetRandom();
+            
+            _patternTween?.Kill();
+            _patternTween = DOTween.Sequence();
+
+            foreach (PatternPart part in pattern.Parts)
+                _patternTween
+                    .AppendInterval(part.Delay + pattern.DelayBetweenEachPart)
+                    .AppendCallback(() =>
+                    {
+                        Quaternion rotation = Quaternion.Euler(0, 0, part.Rotation);
+                        SpawnProjectile(part.Point.position, rotation, part.Point);
+                    });
+            
+            _patternTween
+                .AppendInterval(pattern.DelayAfterPattern)
+                .AppendCallback(SetNewPattern)
+                .SetAutoKill()
+                .Play();
         }
     }
 }
